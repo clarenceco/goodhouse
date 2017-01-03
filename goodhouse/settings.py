623ce55364568310f292c63c75bd19ba691bd6fa
Clarence Co <clarenceco1001@gmail.com>
2017-01-03 16:01:17 +0800

    fix db

--- goodhouse/settings.py
+++ goodhouse/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,17 +77,17 @@ WSGI_APPLICATION = 'goodhouse.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'ghdb',
-        'PORT': '5433',
-        'HOST': 'localhost',
-        'USER': 'goodhouse',
-        'PASSWORD': 'raffy',
-    }
+    'default' : dj_database_url.config()
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #     'NAME': 'ghdb',
+    #     'PORT': '5432',
+    #     'HOST': '127.0.0.1',
+    #     'USER': 'goodhouse',
+    #     'PASSWORD': 'raffy',
+    # }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
