cca2eb35d4b1529437095c2b4693f0e282f9462b
Clarence Co <clarenceco1001@gmail.com>
2017-01-03 14:49:28 +0800

    test deploy2

--- goodhouse/settings.py
+++ goodhouse/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e^buj@u!+$_0lrrrg6+=(x@m@@)%l)l9tz8_&fbz+gbn-grve3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
