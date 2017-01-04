2788738d7e55f8b3a601f8092f3f0f769cf4a2fd
Clarence Co <clarenceco1001@gmail.com>
2017-01-05 00:12:21 +0800

    unicode literals

--- gh_app/views.py
+++ gh_app/views.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals 
 from django.shortcuts import render
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from __future__ import unicode_literals 
+
 
 from .models import Post, AirWater_Filter, AirWater_EquipmentConsumable, Career
 
