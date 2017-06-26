c74f7441fe429cc22491271dedcc64e2c02bcf05
Clarence Co <clarenceco1001@gmail.com>
2017-06-26 13:43:25 +0800

    product catalog

--- gh_app/admin.py
+++ gh_app/admin.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals 
 from django.contrib import admin
-from .models import Post, Air_Filter, Water_Filter, Dust_Filter, Featured_Product, Career, Service
+from .models import Post, Air_Filter, Water_Filter, Dust_Filter, Featured_Product, Career, Service, ProductCatalog
 
 admin.site.register(Post)
 admin.site.register(Career)
@@ -9,4 +9,5 @@ admin.site.register(Water_Filter)
 admin.site.register(Dust_Filter)
 admin.site.register(Service)
 admin.site.register(Featured_Product)
+admin.site.register(ProductCatalog)
 # Register your models here.
